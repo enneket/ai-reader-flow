a7121fc3bb42821c133633028a880475bcbe488d
enneket <zhao.jiaxing@qq.com>
2026-03-21 19:47:50 +0800

    fix: remove water.css and use App.css for new design system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {HashRouter} from 'react-router-dom'
-import './style.css'
+import './App.css'
 import App from './App'
 
 const container = document.getElementById('root')
